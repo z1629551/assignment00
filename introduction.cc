63472083068b8fc9a550ca4d0bb6edd187a33db6
Brenna Hatfield <70289085+z1629551@users.noreply.github.com>
2020-09-06 23:59:12 -0500

    Create introduction.cc

---
+++ introduction.cc
@@ -0,0 +1,16 @@
+// Insert "Doc-Box" here
+#include <iostream>
+
+using namespace std;
+
+int
+main(int argc, char *argv[])
+{
+
+	// Insert new code line replacing this comment.
+	
+	cout << "I'm very excited to be taking Data Structures" << endl;
+	
+	return 0;
+
+}// End of main() routine.
